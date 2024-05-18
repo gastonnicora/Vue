b5614f45f2b575fe098bd5f1e6c3845e2b356c77
NICORA, GASTON MATIAS <gastonmatias_21@hotmail.com>
2024-05-18 13:31:45 -0300

    cambios para socket

--- src/socket.js
+++ src/socket.js
@@ -10,7 +10,7 @@ let URL
 if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
   URL = 'http://localhost:4000/'
 } else {
-  URL = 'http://localhost:4000/'
+  URL = 'http://api:4000/'
 }
 
 export const socket = io(URL, {
