6c24a3d7fb478a6cd192f38537ac39a47db10515
NICORA, GASTON MATIAS <gastonmatias_21@hotmail.com>
2024-05-18 13:54:36 -0300

    cambios para socket en labs docker

--- src/socket.js
+++ src/socket.js
@@ -10,7 +10,7 @@ let URL
 if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
   URL = 'http://localhost:4000/'
 } else {
-  URL = 'https://api:4000/'
+  URL = 'https://api/'
 }
 
 export const socket = io(URL, {
