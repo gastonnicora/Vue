884602ae73de834366440d4d224ddc1626b42d74
NICORA, GASTON MATIAS <gastonmatias_21@hotmail.com>
2024-05-20 14:57:07 -0300

    s

--- src/socket.js
+++ src/socket.js
@@ -17,7 +17,8 @@ if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
 export const socket = io(URL, {
   transports: ['polling'],
   reconnectionAttempts: 5, // Intentos de reconexión
-  reconnectionDelay: 1000 // Tiempo de espera entre intentos
+  reconnectionDelay: 1000, // Tiempo de espera entre intentos
+  timeout: 20000 // Timeout de conexión
 })
 console.log(socket)
 
