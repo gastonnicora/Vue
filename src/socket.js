749c41019e5cc5dcbe881df7ebdd4f5c852614f4
gastonnicora <gastonmatias.21@gmail.com>
2024-08-07 17:24:49 -0300

    prueba socket

--- src/socket.js
+++ src/socket.js
@@ -15,7 +15,10 @@ if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
 }
 
 export const socket = io(URL, {
-  transports: ['polling']
+  transports: ['polling'],
+  reconnectionAttempts: 5, // Intentos de reconexión
+  reconnectionDelay: 1000, // Tiempo de espera entre intent
+  timeout: 20000 // Timeout de conexión
 })
 console.log(socket)
 
