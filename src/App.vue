55bcd9afd3606272a3a4853c469959d703cdff4c
gastonnicora <gastonmatias.21@gmail.com>
2024-09-20 16:18:27 -0300

    prueba

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <navbar></navbar>
-  <p>State: {{ con }}</p>
+  <!-- <p>State: {{ con }}</p>
   <button @click="connect()">Connect</button>
-  <button @click="disconnect()">Disconnect</button>
-  <!-- <div class="body"><router-view /></div>
+  <button @click="disconnect()">Disconnect</button> -->
+  <div class="body"><router-view /></div>
   <loading
     v-model:active="this.$store.state.isLoading"
     :can-cancel="false"
     :is-full-page="true"
     loader="bars"
-  />-->
+  />
   <Footers></Footers>
 </template>
 <style>
@@ -18,33 +18,33 @@
 @import './assets/app.css';
 </style>
 <script>
-// import Loading from 'vue-loading-overlay'
-// import 'vue-loading-overlay/dist/css/index.css'
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/css/index.css'
 
 import { state, socket } from '@/socket.js'
 import Navbar from '@/components/nav/navbar'
 import Footers from '@/components/nav/footer'
 export default {
   computed: {
-    // connected () {
-    //   const user = this.$store.state.session
-    //   if (user && user.uuid) {
-    //     socket.emit('coneccion', { name: user.name, lastName: user.lastName, uuid: user.uuid, email: user.email })
-    //     console.log('coneccion exitosa')
-    //   }
-    //   return state.connected
-    // },
+    connected () {
+      const user = this.$store.state.session
+      if (user && user.uuid) {
+        socket.emit('coneccion', { name: user.name, lastName: user.lastName, uuid: user.uuid, email: user.email })
+        console.log('coneccion exitosa')
+      }
+      return state.connected
+    },
     con () {
       return state.connected
     }
   },
   async created () {
     this.$store.commit('SET_CONNECTION')
-    // this.inicio()
-    // const user = this.$store.state.session
-    // if (user && user.uuid) {
-    //   socket.emit('coneccion', { name: user.name, lastName: user.lastName, uuid: user.uuid, email: user.email })
-    // }
+    this.inicio()
+    const user = this.$store.state.session
+    if (user && user.uuid) {
+      socket.emit('coneccion', { name: user.name, lastName: user.lastName, uuid: user.uuid, email: user.email })
+    }
   },
   data () {
     return {
@@ -52,16 +52,16 @@ export default {
     }
   },
   components: {
-    // Loading,
+    Loading,
     Navbar,
     Footers
   },
 
   methods: {
-    // inicio () {
-    //   this.$store.state.session = JSON.parse(localStorage.getItem('sesion'))
-    //   this.$store.state.token = JSON.parse(localStorage.getItem('token'))
-    // },
+    inicio () {
+      this.$store.state.session = JSON.parse(localStorage.getItem('sesion'))
+      this.$store.state.token = JSON.parse(localStorage.getItem('token'))
+    },
     connect () {
       socket.connect()
     },
@@ -69,15 +69,15 @@ export default {
       socket.disconnect()
     }
 
+  },
+  mounted () {
+    console.log('con session')
+    socket.on('updateSession', (data) => {
+      console.log(data.data)
+      this.$store.state.session = data.data
+      localStorage.setItem('sesion', JSON.stringify(data.data))
+    })
   }
-  // mounted () {
-  //   console.log('con session')
-  //   socket.on('updateSession', (data) => {
-  //     console.log(data.data)
-  //     this.$store.state.session = data.data
-  //     localStorage.setItem('sesion', JSON.stringify(data.data))
-  //   })
-  // }
 
 }
 </script>
