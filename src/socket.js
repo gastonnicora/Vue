38bcf42a18ac622b3aba9ee15ad5d684019696dc
gastonnicora <gastonmatias.21@gmail.com>
2024-09-21 16:33:47 -0300

    prueba

--- src/socket.js
+++ src/socket.js
@@ -14,7 +14,6 @@ if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
 }
 console.log(URL)
 export const socket = io(URL, {
-  transports: ['websocket']
 })
 
 socket.on('connect', () => {
