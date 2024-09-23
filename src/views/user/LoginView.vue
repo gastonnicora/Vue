b9500b9f5bfd1e50f95a68abe41bcd32f4d92057
gastonnicora <gastonmatias.21@gmail.com>
2024-09-23 14:37:53 -0300

    prueba copiar

--- src/views/user/LoginView.vue
+++ src/views/user/LoginView.vue
@@ -5,8 +5,8 @@
         <p>Esto es una version de prueba. Ya se inicializan algunos usuarios, remates, artículos y pujas.</p>
         <p>Usuario de prueba:
           <ul>
-            <li >Email:<div class="li-copy" @click="copy('email')"> <copy id="email">usuario@example.com</copy><font-awesome-icon id="icon_email" class="icon-copy" :icon="['far', 'copy']" /><font-awesome-icon id="check_email" class="icon-copy" :icon="['fas', 'check']" style="display:none ; color:#3a3" /> </div></li>
-            <li >Contraseña:<div class="li-copy" @click="copy('pass')"> <copy id="pass">12345678</copy><font-awesome-icon id="icon_pass" class="icon-copy" :icon="['far', 'copy']" /><font-awesome-icon id="check_pass" class="icon-copy" :icon="['fas', 'check']" style="display:none ; color:#3a3" /> </div></li>
+            <li >Email:<div class="li-copy" @click="copy('email')"> <copy id="emailc">usuario@example.com</copy><font-awesome-icon id="icon_emailc" class="icon-copy" :icon="['far', 'copy']" /><font-awesome-icon id="check_emailc" class="icon-copy" :icon="['fas', 'check']" style="display:none ; color:#3a3" /> </div></li>
+            <li >Contraseña:<div class="li-copy" @click="copy('pass')"> <copy id="passc">12345678</copy><font-awesome-icon id="icon_passc" class="icon-copy" :icon="['far', 'copy']" /><font-awesome-icon id="check_passc" class="icon-copy" :icon="['fas', 'check']" style="display:none ; color:#3a3" /> </div></li>
           </ul>
         </p>
       </div>
@@ -37,23 +37,13 @@ export default {
       this.login = state
     },
     copy (id) {
-      const copyText = document.getElementById(id)
+      const copyText = document.getElementById(id + 'c')
+      const pasteText = document.getElementById(id)
       const icon = document.getElementById('icon_' + id)
       const check = document.getElementById('check_' + id)
       icon.style.display = 'none'
       check.style.display = 'block'
-      if (navigator.clipboard && copyText) {
-        navigator.clipboard.writeText(copyText.innerHTML)
-          .then(() => {
-            console.log('Texto copiado al portapapeles')
-          })
-          .catch(err => {
-            console.error('Error al copiar: ', err)
-          })
-      } else {
-        console.error('Clipboard API no está disponible')
-      }
-      navigator.clipboard.writeText(copyText.innerHTML)
+      pasteText.value = copyText.innerHTML
 
       setTimeout(() => {
         icon.style.display = 'block'
